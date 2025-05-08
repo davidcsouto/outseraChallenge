deb14d02a1717484b446707f0cc4a556e97ee608
David Souto <csouto.david@gmail.com>
2025-05-08 12:14:20 -0300

    removendo tag do RunnerApi

--- src/main/java/runner/RunnerApi.java
+++ src/main/java/runner/RunnerApi.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "classpath:features/Api",
         glue = "classpath:api",
-        tags = "@DeletarReserva",
+        tags = "",
         plugin = {"json:target/cucumber-report/cucumber.json"}
 )
 
